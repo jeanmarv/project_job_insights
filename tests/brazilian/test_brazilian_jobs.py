1843909757c724f344abe03a9d97c0c1eb5d6ecb
Jean Marcel <jeanvanzella91@gmail.com>
2022-08-18 15:35:00 -0300

    req 11, test for function read_brazilian_files

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    data = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    for jobs in data:
+        assert "title" in jobs
+        assert "salary" in jobs
+        assert "type" in jobs
