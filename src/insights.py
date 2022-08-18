a3c0f030bbd17ff2410c0a06f85ceb9448c68213
Jean Marcel <jeanvanzella91@gmail.com>
2022-08-18 01:51:03 -0300

    req 9, filter salary range

--- src/insights.py
+++ src/insights.py
@@ -78,44 +78,14 @@ def matches_salary_range(job, salary):
         return min_salary <= salary <= max_salary
     except KeyError:
         raise ValueError
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    jobs_list = []
+    for alljobs in jobs:
+        try:
+            if matches_salary_range(alljobs, salary):
+                jobs_list.append(alljobs)
+        except ValueError:
+            pass
+    return jobs_list
